7af773484ea0d2e0987abc6d9abe4d1c155b54b0
Marcin Miazga <miazgamarcin@outlook.com>
2020-03-21 15:32:34 +0100

    Add reverse proxy to host rabbitmq management in /rabbit path

---
+++ server/src/Server.Api/Middleware/RabbitMqManagementHost.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Server.Api.Middleware
+{
+    public class RabbitMqManagementHost
+    {
+        private const string RabbitMqUri = "http://localhost:15672";
+        
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly RequestDelegate _nextMiddleware;
+
+        public RabbitMqManagementHost(RequestDelegate nextMiddleware)
+        {
+            _nextMiddleware = nextMiddleware;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var targetUri = BuildTargetUri(context.Request);
+
+            if (targetUri != null)
+            {
+                var targetRequestMessage = CreateTargetMessage(context, targetUri);
+
+                using (var responseMessage = await _httpClient.SendAsync(targetRequestMessage,
+                    HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
+                {
+                    context.Response.StatusCode = (int) responseMessage.StatusCode;
+                    CopyFromTargetResponseHeaders(context, responseMessage);
+                    await ProcessResponseContent(context, responseMessage);
+                }
+
+                return;
+            }
+
+            await _nextMiddleware(context);
+        }
+
+        private async Task ProcessResponseContent(HttpContext context, HttpResponseMessage responseMessage)
+        {
+            var content = await responseMessage.Content.ReadAsByteArrayAsync();
+
+            if (IsContentOfType(responseMessage, "text/html") ||
+                IsContentOfType(responseMessage, "text/javascript"))
+            {
+                var stringContent = Encoding.UTF8.GetString(content);
+                await context.Response.WriteAsync(stringContent, Encoding.UTF8);
+            }
+            else
+            {
+                await context.Response.Body.WriteAsync(content);
+            }
+        }
+
+        private bool IsContentOfType(HttpResponseMessage responseMessage, string type)
+        {
+            var result = false;
+
+            if (responseMessage.Content?.Headers?.ContentType != null)
+                result = responseMessage.Content.Headers.ContentType.MediaType == type;
+
+            return result;
+        }
+
+        private HttpRequestMessage CreateTargetMessage(HttpContext context, Uri targetUri)
+        {
+            var requestMessage = new HttpRequestMessage();
+            CopyFromOriginalRequestContentAndHeaders(context, requestMessage);
+
+            requestMessage.RequestUri = targetUri;
+            requestMessage.Headers.Host = targetUri.Host;
+            requestMessage.Method = GetMethod(context.Request.Method);
+
+            return requestMessage;
+        }
+
+        private void CopyFromOriginalRequestContentAndHeaders(HttpContext context, HttpRequestMessage requestMessage)
+        {
+            var requestMethod = context.Request.Method;
+
+            if (!HttpMethods.IsHead(requestMethod) &&
+                !HttpMethods.IsDelete(requestMethod) &&
+                !HttpMethods.IsTrace(requestMethod))
+            {
+                var streamContent = new StreamContent(context.Request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            foreach (var header in context.Request.Headers)
+            {
+                var key = header.Key;
+                var value = header.Value.ToString();
+                requestMessage.Headers.TryAddWithoutValidation(key, value);
+            }
+        }
+
+        private void CopyFromTargetResponseHeaders(HttpContext context, HttpResponseMessage responseMessage)
+        {
+            foreach (var header in responseMessage.Headers)
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+
+            foreach (var header in responseMessage.Content.Headers)
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            context.Response.Headers.Remove("transfer-encoding");
+        }
+
+        private static HttpMethod GetMethod(string method)
+        {
+            if (HttpMethods.IsDelete(method)) return HttpMethod.Delete;
+            if (HttpMethods.IsGet(method)) return HttpMethod.Get;
+            if (HttpMethods.IsHead(method)) return HttpMethod.Head;
+            if (HttpMethods.IsOptions(method)) return HttpMethod.Options;
+            if (HttpMethods.IsPost(method)) return HttpMethod.Post;
+            if (HttpMethods.IsPut(method)) return HttpMethod.Put;
+            if (HttpMethods.IsTrace(method)) return HttpMethod.Trace;
+            return new HttpMethod(method);
+        }
+
+        private Uri BuildTargetUri(HttpRequest request)
+        {
+            Uri targetUri = null;
+
+            var referer = request.Headers["Referer"].ToString();
+            var path = request.Path;
+
+            if (request.Path.StartsWithSegments("/rabbit", out var remainingPath))
+                return new Uri(RabbitMqUri + remainingPath);
+
+            if ((path.StartsWithSegments("/api") || path.StartsWithSegments("/js") ||
+                 path.StartsWithSegments("/css") || path.StartsWithSegments("/img")) && referer.Contains("rabbit"))
+                return new Uri(RabbitMqUri + path.Value);
+
+            return targetUri;
+        }
+    }
+}
--- server/src/Server.Api/Startup.cs
+++ server/src/Server.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Server.Api.Middleware;
 using Server.Api.Persistence.MongoDb;
 using Server.Api.RealTimeUpdates;
 using Server.Api.Security;
@@ -67,6 +68,8 @@ namespace Server.Api
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Reference V1");
                 c.RoutePrefix = "docs";
             });
+            
+            app.UseMiddleware<RabbitMqManagementHost>();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
